50f6e6bbb9e1918f4573e272241f29fc8394d866
Kenichi Ito <ken.i54k@gmail.com>
2019-06-17 20:54:50 +0900

    Define main module

--- jatr/__init__.py
+++ jatr/__init__.py
@@ -1,10 +1,14 @@
 """日本語テキスト難易度推定"""
 
 import jatr.vocab as jv
+import fileinput
 
 
 def main():
-    v = jv.Vocab('李さんは毎日お酒をのんでいます。')
+    text = ''.join([line for line in fileinput.input()])
+
+    v = jv.Vocab(text)
+    # v = jv.Vocab('李さんは毎日お酒をのんでいます。')
     print('語彙の難易度')
     print('平均値:', v.mean)
     print('最大値:', v.max)
