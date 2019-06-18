c0b8cbf27ee6fa9692f259063c41ac6f3b627dfc
Kenichi Ito <ken.i54k@gmail.com>
2019-06-18 22:28:33 +0900

    Improve metrics display

--- jatr/__init__.py
+++ jatr/__init__.py
@@ -10,18 +10,15 @@ def main():
     text = ''.join([line.rstrip() for line in fileinput.input()])
     sentences = re.findall(r'[^。]+(?:[。]|$)', text)
 
+    vt = jv.Vocab(text)
+    print('文章全体')
+    print('文数:', len(sentences))
+    vt.show_metrics()
+
     for sentence in sentences:
         v = jv.Vocab(sentence)
-        print(v.sentence)
-        print('文長:', v.len)
-        print('語彙の難易度')
-        print('平均値:', v.mean)
-        print('最大値:', v.max)
-        print('中央値:', v.median)
-        print('最頻値:', v.mode)
-        print('標準偏差:', v.stdev)
-        print('分散:', v.variance)
-        print()
+        print(sentence)
+        v.show_metrics()
 
 
 if __name__ == '__main__':
--- jatr/vocab.py
+++ jatr/vocab.py
@@ -11,15 +11,27 @@ from jatr.util import abs_path
 
 
 class Vocab:
-    def __init__(self, sentence: str):
-        self.sentence = sentence
-        self.len = len(self.sentence)
+    def __init__(self, text: str):
+        self.text = text
+        self.len = len(self.text)
         self.vocab = self.load_vocab()
-        self.words = self.split_into_words(self.sentence)
+        self.words = self.split_into_words(self.text)
         self.level_list = self.calc_vocab_level(self.words)
-        self.mean = st.mean(self.level_list)
-        self.max = max(self.level_list)
-        self.median = st.median(self.level_list)
+
+        try:
+            self.max = max(self.level_list)
+        except ValueError:
+            self.max = math.nan
+
+        try:
+            self.mean = st.mean(self.level_list)
+        except st.StatisticsError:
+            self.mean = math.nan
+
+        try:
+            self.median = st.median(self.level_list)
+        except st.StatisticsError:
+            self.median = math.nan
 
         try:
             self.mode = st.mode(self.level_list)
@@ -39,13 +51,24 @@ class Vocab:
         edu_vocab['語彙の難易度'] = edu_vocab['語彙の難易度'].map(lambda s: s[0]).astype('int')
         return edu_vocab
 
-    def split_into_words(self, sentence: str) -> List[str]:
+    def split_into_words(self, text: str) -> List[str]:
         """分かち書きにしてリストで返す"""
         m = MeCab.Tagger("-Owakati")
-        return m.parse(sentence).split(" ")
+        return m.parse(text).split(" ")
 
     def calc_vocab_level(self, words: List[str]) -> List[int]:
         level_list: List[int] = []
         for word in words:
             level_list += self.vocab[self.vocab['標準的な表記'] == word]['語彙の難易度'].to_list()
         return level_list
+
+    def show_metrics(self):
+        print('文字数:', self.len)
+        print('語彙の難易度')
+        print('平均値:', self.mean)
+        print('最大値:', self.max)
+        print('中央値:', self.median)
+        print('最頻値:', self.mode)
+        print('標準偏差:', self.stdev)
+        print('分散:', self.variance)
+        print()
