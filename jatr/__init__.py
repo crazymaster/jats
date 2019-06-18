0f475ba58d47704169fa0ad30d87f2f11e439b23
Kenichi Ito <ken.i54k@gmail.com>
2019-06-18 16:14:45 +0900

    Split to sentence

--- jatr/__init__.py
+++ jatr/__init__.py
@@ -2,20 +2,26 @@
 
 import jatr.vocab as jv
 import fileinput
+import functools
+import re
 
 
 def main():
-    text = ''.join(fileinput.input())
+    texts = re.split('[。！？!?]', functools.reduce(str.rstrip, fileinput.input()))
+    # texts = ''.join([line.rstrip() for line in fileinput.input()]).split('。')
 
-    v = jv.Vocab(text)
-    # v = jv.Vocab('李さんは毎日お酒をのんでいます。')
-    print('語彙の難易度')
-    print('平均値:', v.mean)
-    print('最大値:', v.max)
-    print('中央値:', v.median)
-    print('最頻値:', v.mode)
-    print('標準偏差:', v.stdev)
-    print('分散:', v.variance)
+    for text in texts:
+        v = jv.Vocab(text)
+        # v = jv.Vocab('李さんは毎日お酒をのんでいます。')
+        print(v.text)
+        print('語彙の難易度')
+        print('平均値:', v.mean)
+        print('最大値:', v.max)
+        print('中央値:', v.median)
+        print('最頻値:', v.mode)
+        print('標準偏差:', v.stdev)
+        print('分散:', v.variance)
+        print()
 
 
 if __name__ == '__main__':
